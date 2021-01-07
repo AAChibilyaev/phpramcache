60778642fc2a9063b2c72589a779f38d7fbfa557
AAChibilyaev <info@aachibilyaev.com>
2021-01-07 11:12:29 +0300

    cache class

---
+++ src/Ramcache.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * Copyright (c) 2021.
+ * Chibilyaev Alexandr
+ * info@aachibilyaev.com
+ * https://aachibilyaev.com
+ */
+
+namespace aachibilyaev\phpramcache;
+
+/**
+ * Class Phpramcache
+ *
+ * @package aachibilyaev\phpramcache
+ */
+class Phpramcache{
+		/**
+		 * @var string
+		 */
+		private $ramFilesPath = '';
+
+		/**
+		 * Ramcache constructor.
+		 *
+		 * @param string $ramFilesPath
+		 */
+		public function __construct($ramFilesPath = '/tmp/') { $this->ramFilesPath = $ramFilesPath; }
+
+		/**
+		 * @return string
+		 */
+		public function getRamFilesPath()
+		{
+				return $this->ramFilesPath;
+		}
+
+		/**
+		 * @param string $ramFilesPath
+		 */
+		public function setRamFilesPath($ramFilesPath)
+		{
+				$this->ramFilesPath = $ramFilesPath;
+		}
+
+		/**
+		 * @param $key
+		 * @param $val
+		 */
+		public function setStorage($key, $val) {
+				$val = var_export($val, true);
+				$val = str_replace('stdClass::__set_state', '(object)', $val);
+				$tmp = $this->getRamFilesPath().$key. uniqid('', true) . '.tmp';
+				file_put_contents($tmp, '<?php $val = ' . $val . ';', LOCK_EX);
+				rename($tmp, $this->getRamFilesPath().$key);
+				unset($val);
+				unset($tmp);
+		}
+
+		/**
+		 * @param $key
+		 *
+		 * @return false|mixed
+		 */
+		public function getStorage($key) {
+				@include "/tmp/$key";
+				return isset($val) ? $val : false;
+		}
+}
